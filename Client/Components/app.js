c2eec62dd91d0e3ed0208bd3a4444834f9c0623b
brianlee <bnlee13@gmail.com>
2017-04-08 19:57:59 -0400

    watson data set to json parsed

--- Client/Components/app.js
+++ Client/Components/app.js
@@ -2,10 +2,49 @@
 "use strict";
 var React = require('react');
 
+var Map = require('react-d3-map').Map;
+// require your <Marker> component
+var MarkerGroup = require('react-d3-map').MarkerGroup;
+
+var data = {
+    "type": "Feature",
+    "properties": {
+      "text": "this is a Point!!!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [122, 23.5]
+    }
+}
+
+var width = 700;
+var height = 700;
+// set your zoom scale
+var scale = 1200 * 5;
+// min and max of your zoom scale
+var scaleExtent = [1 << 12, 1 << 13]
+// set your center point
+var center = [122, 23.5];
+// set your popupContent
+var popupContent = function(d) { return d.properties.text; }
+
 var ReactApp = React.createClass({
       render: function () {
         return (
-          <h1>Hello</h1>
+          <Map
+            width= {width}
+            height= {height}
+            scale= {scale}
+            scaleExtent= {scaleExtent}
+            center= {center}
+          >
+            <MarkerGroup
+              key= {"polygon-test"}
+              data= {data}
+              popupContent= {popupContent}
+              markerClass= {"your-marker-css-class"}
+            />
+          </Map>
         )
       }
   });
